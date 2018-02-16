cdb84b028ab07a904393076718906d4ba6c9638d
Brisanocturna <ramon.soler.hernan@gmail.com>
2018-02-16 12:19:49 +0100

    layout complete for show news

--- app/src/main/java/com/example/ramon/proyectofinalramonsolerhernan/MostrarNoticiaActivity.java
+++ app/src/main/java/com/example/ramon/proyectofinalramonsolerhernan/MostrarNoticiaActivity.java
@@ -2,16 +2,35 @@ package com.example.ramon.proyectofinalramonsolerhernan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.ramon.proyectofinalramonsolerhernan.POJOS.Noticias;
 
 public class MostrarNoticiaActivity extends AppCompatActivity {
-
+    public Toolbar toolbar;
     TextView titulo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_noticia);
         titulo = findViewById(R.id.txtMostrarTituloNoticia);
+        toolbar = findViewById(R.id.toolbarNoticiasMostrar);
+        setSupportActionBar(toolbar);
         Bundle b = getIntent().getExtras();
+        Noticias n = b.getParcelable("NOTICIA");
+        titulo.setText(n.getTitulo().toString());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mostrar_noticia,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
